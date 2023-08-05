0f6f028f9fcdeaa5c4f22112d56e6879d9c23008
Toxe98 <111653899+Toxe98@users.noreply.github.com>
2023-08-05 17:59:44 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -1,3 +1,8 @@
-document.querySelector("div").addEventListener("click", function() {
-    alert("Hola! Soy el div");
+document.getElementById("botonSaludo").addEventListener('click', function(e){
+    e.stopPropagation();
+    alert("Hola!");
+});
+
+document.getElementById("divSaludo").addEventListener('click', function(){
+    alert("Hola! Soy el div")
 })
