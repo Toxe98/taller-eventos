c066532a0d57e8a6d10e4523ecb4430df2f15155
Toxe98 <111653899+Toxe98@users.noreply.github.com>
2023-08-05 18:11:07 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -1,11 +1,8 @@
-
-document.getElementById('divSaludo').addEventListener('click', function(e){
+document.getElementById('botonSaludo').addEventListener('click', function(e){
     e.stopPropagation();
-    alert("Hola! Soy el div")
-});
-
-document.getElementById('botonSaludo').addEventListener('click', function(){
-    
     alert("Hola!");
 });
 
+document.getElementById('divSaludo').addEventListener('click', function(){
+    alert("Hola! Soy el div")
+});
